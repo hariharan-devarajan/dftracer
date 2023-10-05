f61b430151eb3eaa36b4424c115e99a8794991fd
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-10-05 15:39:54 -0700

    make LD_PRELOAD style of DLIO Profiler.

--- src/dlio_profiler/dlio_profiler.cpp
+++ src/dlio_profiler/dlio_profiler.cpp
@@ -9,6 +9,9 @@
 #include<algorithm>
 #include <dlio_profiler/dlio_logger.h>
 #include <dlio_profiler/utils/utils.h>
+#include <dlio_profiler/core/constants.h>
+#include <dlio_profiler/core/dlio_profiler_main.h>
+
 namespace dlio_profiler {
     bool init = false;
 }
@@ -16,75 +19,18 @@ namespace dlio_profiler {
 bool is_init() {return dlio_profiler::init;}
 void set_init(bool _init) { dlio_profiler::init = _init;}
 void dlio_profiler_init(void) {
-  bool init_log = false;
-  char *ld_preload = getenv("LD_PRELOAD");
-  if (ld_preload != nullptr && std::string(ld_preload).find("libdlio_profiler.so") != std::string::npos) {
-    init_log = true;
-  }
-  if (!is_init()) {
-    char *dlio_profiler_enable = getenv("DLIO_PROFILER_ENABLE");
-    if (dlio_profiler_enable == nullptr || strcmp(dlio_profiler_enable, "1") == 0) {
-      char *dlio_profiler_debug = getenv("DLIO_PROFILER_DEBUG");
-      if (dlio_profiler_debug != nullptr) {
-        if (strcmp(dlio_profiler_debug, "1") == 0) {
-          std::string sp;
-          std::ifstream("/proc/self/cmdline") >> sp;
-          std::replace(sp.begin(), sp.end() - 1, '\000', ' ');
-          fprintf(stderr, "Connect to pid %d %s\n", getpid(), sp.c_str());
-          fflush(stderr);
-          getchar();
-        }
-      }
-      DLIO_PROFILER_LOGINFO("constructor", "");
-      char *dlio_profiler_log_level = getenv("DLIO_PROFILER_LOG_LEVEL");
-      if (dlio_profiler_log_level == nullptr) {
-        DLIO_PROFILER_LOGGER->level = cpplogger::LoggerType::LOG_ERROR;
-        DLIO_PROFILER_LOGINFO("Enabling ERROR loggin", "");
-      } else {
-        if (strcmp(dlio_profiler_log_level, "ERROR") == 0) {
-          DLIO_PROFILER_LOGGER->level = cpplogger::LoggerType::LOG_ERROR;
-          DLIO_PROFILER_LOGINFO("Enabling ERROR loggin", "");
-        } else if (strcmp(dlio_profiler_log_level, "INFO") == 0) {
-          DLIO_PROFILER_LOGGER->level = cpplogger::LoggerType::LOG_INFO;
-          DLIO_PROFILER_LOGINFO("Enabling INFO loggin", "");
-        } else if (strcmp(dlio_profiler_log_level, "DEBUG") == 0) {
-          DLIO_PROFILER_LOGINFO("Enabling DEBUG loggin", "");
-          DLIO_PROFILER_LOGGER->level = cpplogger::LoggerType::LOG_WARN;
-        }
-      }
-      char *dlio_profiler_priority_str = getenv("DLIO_PROFILER_GOTCHA_PRIORITY");
-      int dlio_profiler_priority = 1;
-      if (dlio_profiler_priority_str != nullptr) {
-        dlio_profiler_priority = atoi(dlio_profiler_priority_str);
-      }
-      /*
-      dlio_profiler::Singleton<DLIOLogger>::get_instance(init_log);
-      brahma_gotcha_wrap("dlio_profiler", dlio_profiler_priority);
-      auto posix_instance = brahma::POSIXDLIOProfiler::get_instance();
-      auto stdio_instance = brahma::STDIODLIOProfiler::get_instance();
-      char *dlio_profiler_dir = getenv("DLIO_PROFILER_DIR");
-      if (dlio_profiler_dir != nullptr) {
-        auto paths = split(dlio_profiler_dir, ':');
-        for (const auto &path:paths) {
-          DLIO_PROFILER_LOGINFO("Profiler will trace %s\n", path.c_str());
-          posix_instance->trace(path);
-          stdio_instance->trace(path);
-        }
-      }
-       */
-    }
+  char *init_type = getenv(DLIO_PROFILER_INIT);
+  if (!is_init() && init_type != nullptr && strcmp(init_type, "PRELOAD") == 0) {
+    dlio_profiler::Singleton<dlio_profiler::DLIOProfiler>::get_instance(true);
+    DLIO_PROFILER_LOGINFO("Running initialize within constructor", "");
     set_init(true);
   }
-  size_t thread_hash = std::hash<std::thread::id>{}(std::this_thread::get_id());
-  DLIO_PROFILER_LOGINFO("Running DLIO Profiler on thread %ld and pid %ld", thread_hash, getpid());
 
 }
 void dlio_profiler_fini(void) {
-  if (is_init()) {
-    char *dlio_profiler_init = getenv("DLIO_PROFILER_INIT");
-    if (dlio_profiler_init == nullptr || strcmp(dlio_profiler_init, "1") == 0) {
-      free_bindings();
-    }
+  char *init_type = getenv(DLIO_PROFILER_INIT);
+  if (is_init() && init_type != nullptr && strcmp(init_type, "PRELOAD")) {
+    dlio_profiler::Singleton<dlio_profiler::DLIOProfiler>::get_instance(false)->finalize();
     set_init(false);
   }
 }
