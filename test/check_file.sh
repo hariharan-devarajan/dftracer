918fd7569e50664514f9cd13a28c1539342933fb
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-10-08 13:10:28 -0700

    fixes to improve test cases.

--- test/check_file.sh
+++ test/check_file.sh
@@ -3,7 +3,7 @@ expected_lines=$2
 echo "cat $1 | grep POSIX | wc -l"
 num_posix_lines=$(cat $1 | wc -l)
 echo $num_posix_lines
-if [[ "$num_posix_lines" -ne "$expected_lines" ]]; then
+if [[ "$num_posix_lines" -lt "$expected_lines" ]]; then
   cat $1
   exit 1
 fi
