f2a1fe13725e9bd41f46fa86b429d451266849fb
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-10-08 15:10:46 -0700

    code refactoring

--- src/dlio_profiler/dlio_profiler_preload.cpp
+++ src/dlio_profiler/dlio_profiler_preload.cpp
@@ -14,9 +14,15 @@ namespace dlio_profiler {
 bool is_init() {return dlio_profiler::init;}
 void set_init(bool _init) { dlio_profiler::init = _init;}
 void dlio_profiler_init(void) {
+  if (!is_init()) {
     dlio_profiler::Singleton<dlio_profiler::DLIOProfilerCore>::get_instance(ProfilerStage::PROFILER_INIT, ProfileType::PROFILER_PRELOAD);
+    set_init(true);
+  }
 }
 void dlio_profiler_fini(void) {
+  if (is_init()) {
     dlio_profiler::Singleton<dlio_profiler::DLIOProfilerCore>::get_instance(ProfilerStage::PROFILER_FINI, ProfileType::PROFILER_PRELOAD)->finalize();
+    set_init(false);
+  }
 
 }
