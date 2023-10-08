e81956082820f1eb08747d43147a62ead6cdfe02
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-10-08 13:30:10 -0700

    added more ci testing.

--- test/check_file.sh
+++ test/check_file.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 expected_lines=$2
 echo "cat $1 | grep POSIX | wc -l"
-num_posix_lines=$(cat $1 | wc -l)
+num_posix_lines=$(cat $1 | wc -l 2> /dev/null)
 echo $num_posix_lines
+rm -r $1 2> /dev/null
 if [[ "$num_posix_lines" -lt "$expected_lines" ]]; then
-  cat $1
   exit 1
 fi
-rm $1
