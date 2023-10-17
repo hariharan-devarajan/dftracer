7a81f56909c6c5c6f12d8b2618cbd9b8252faeb5
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-10-16 23:30:29 -0700

    Update typedef.h

--- include/dlio_profiler/core/typedef.h
+++ include/dlio_profiler/core/typedef.h
@@ -4,5 +4,5 @@
 
 #ifndef DLIO_PROFILER_TYPEDEF_H
 #define DLIO_PROFILER_TYPEDEF_H
-typedef unsigned long int TimeResolution;
+typedef unsigned long long int TimeResolution;
 #endif //DLIO_PROFILER_TYPEDEF_H
