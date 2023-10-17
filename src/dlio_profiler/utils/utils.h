5c439d5639a591b93de4d6e3479f772b45f65333
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-10-16 22:47:28 -0700

    improved printing of backtrace

--- src/dlio_profiler/utils/utils.h
+++ src/dlio_profiler/utils/utils.h
@@ -17,116 +17,31 @@
 #include <sstream>
 #include <regex>
 
-inline std::string getexepath() { // GCOVR_EXCL_START
-  char result[PATH_MAX];
-  ssize_t count = readlink("/proc/self/exe", result, PATH_MAX);
-  return std::string(result, (count > 0) ? count : 0);
-}
-
-inline std::string sh(std::string cmd) {
-  std::array<char, 128> buffer;
-  std::string result;
-  std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
-  if (!pipe) throw std::runtime_error("popen() failed!");
-  while (!feof(pipe.get())) {
-    if (fgets(buffer.data(), 128, pipe.get()) != nullptr) {
-      result += buffer.data();
-    }
-  }
-  return result;
-}
-
-inline void print_backtrace(void) {
-  void *bt[1024];
-  int bt_size;
-  char **bt_syms;
-  int i;
-
-  bt_size = backtrace(bt, 1024);
-  bt_syms = backtrace_symbols(bt, bt_size);
-  std::regex re("\\[(.+)\\]");
-  auto exec_path = getexepath();
-  std::string addrs = "";
-  for (i = 1; i < bt_size; i++) {
-    std::string sym = bt_syms[i];
-    std::smatch ms;
-    if (std::regex_search(sym, ms, re)) {
-      std::string m = ms[1];
-      addrs += " " + m;
-    }
-  }
-  auto r = sh("addr2line -e " + exec_path + " -f -C " + addrs);
-  std::cout << r << std::endl;
-  free(bt_syms);
-}
-
 void dlio_finalize();
 
 inline void signal_handler(int sig) {
   switch (sig) {
-    case SIGHUP: {
-      DLIO_PROFILER_LOGDEBUG("hangup signal caught", 0);
-      break;
-    }
     case SIGTERM: {
       DLIO_PROFILER_LOGDEBUG("terminate signal caught", 0);
       dlio_finalize();
       exit(1);
       break;
     }
-    case SIGINT: {
-      DLIO_PROFILER_LOGDEBUG("terminate signal caught", 0);
-      dlio_finalize();
-      exit(sig);
-      break;
-    }
     default: {
       DLIO_PROFILER_LOGINFO("signal caught %d", sig);
       dlio_finalize();
-      //print_backtrace();
-      void *array[20];
-      size_t size;
-      void *trace[16];
-      char **messages = (char **) NULL;
-      int i, trace_size = 0;
+      int j, nptrs;
+      void *buffer[20];
+      char **strings;
 
-      size = backtrace(array, 20);
-      messages = backtrace_symbols(array, size);
-      /* skip first stack frame (points here) */
-      std::stringstream myString;
-      myString << "[bt] Execution path with signal " << sig << ":\n";
-      for (i = 1; i < size; ++i) {
-        //printf("%s\n", messages[i]);
-        std::string m_string(messages[i]);
-        //./prog(myfunc3+0x5c) [0x80487f0]
-        std::size_t open_paren = m_string.find_first_of("(");
-        if (open_paren == std::string::npos) {
-          myString << "[bt] #" << i << " " << messages[i] << "\n";
-          continue;
-        }
-        std::size_t plus = m_string.find_first_of("+");
-        std::size_t close_paren = m_string.find_first_of(")");
-        std::size_t open_square = m_string.find_first_of("[");
-        std::size_t close_square = m_string.find_first_of("]");
-        std::string prog_name = m_string.substr(0, open_paren);
-        std::string func_name = m_string.substr(open_paren + 1, plus - open_paren - 1);
-        std::string offset = m_string.substr(plus + 1, close_paren - plus - 1);
-        std::string addr = m_string.substr(open_square + 1, close_square - open_square - 1);
-        if (func_name.empty()) {
-          myString << "[bt] #" << i << " " << messages[i] << "\n";
-          continue;
-        }
-        char command[256];
-        sprintf(command, "nm %s | grep \"\\s%s$\" | awk '{print $1}'", prog_name.c_str(), func_name.c_str());
-        std::string base_addr = sh(command);
-        sprintf(command, "python -c \"print(hex(0x%s+%s))\"", base_addr.c_str(), offset.c_str());
-        std::string hex_val = sh(command);
-        sprintf(command, "addr2line -e %s %s", prog_name.c_str(), hex_val.c_str());
-        std::string line = sh(command); // line has a new line char already
-        myString << "[bt] #" << i << " " << prog_name << "(" << func_name << "+" << offset << ")" << line;
+      nptrs = backtrace(buffer, 20);
+      strings = backtrace_symbols(buffer, nptrs);
+      if (strings != NULL) {
+        for (j = 0; j < nptrs; j++)
+          printf("%s\n", strings[j]);
+
+        free(strings);
       }
-      std::string res = myString.str();
-      std::cout << res;
       ::raise(SIGTERM);
     }
 
