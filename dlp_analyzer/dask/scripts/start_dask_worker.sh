95a466b5143a4506288cebb0f217b576d5c62ac9
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-11-02 09:51:49 -0700

    fixes for analyzer.

--- dlp_analyzer/dask/scripts/start_dask_worker.sh
+++ dlp_analyzer/dask/scripts/start_dask_worker.sh
@@ -8,12 +8,12 @@ export PYTHONPATH=${DLIO_PROFILER_APP}:${PYTHONPATH}
 # This is start of every script.
 source ${DLIO_PROFILER_APP}/dlp_analyzer/dask/scripts/utils.sh
 eval $(parse_yaml $DLIO_PROFILER_DASK_CONF_NAME DLIO_PROFILER_)
-DLIO_PROFILER_JOB_ID=${!DLIO_PROFILER_CONFIG_JOB_ID}
+DLIO_PROFILER_JOB_ID=${!DLIO_PROFILER_JOB_ENV_ID}
 while :
 do
-${DLIO_PROFILER_CONFIG_EXEC_CMD} dask worker --scheduler-file ${DLIO_PROFILER_CONFIG_RUN_DIR}/scheduler.json \
-          --local-directory ${DLIO_PROFILER_CONFIG_WORKER_LOCAL_DIR} \
-          --nworkers ${DLIO_PROFILER_CONFIG_WORKER_PER_CORE} --nthreads ${DLIO_PROFILER_CONFIG_THREADS_PER_WORKER} > ${DLIO_PROFILER_CONFIG_LOG_DIR}/worker_${DLIO_PROFILER_JOB_ID}.log 2>&1
+${DLIO_PROFILER_WORKER_CMD} dask worker --scheduler-file ${DLIO_PROFILER_CONFIG_RUN_DIR}/scheduler.json \
+          --local-directory ${DLIO_PROFILER_WORKER_LOCAL_DIR} \
+          --nworkers ${DLIO_PROFILER_WORKER_PER_CORE} --nthreads ${DLIO_PROFILER_WORKER_THREADS} > ${DLIO_PROFILER_CONFIG_LOG_DIR}/worker_${DLIO_PROFILER_JOB_ENV_ID}.log 2>&1
 echo "Workers existed. Restarting in 1 second"
 sleep 1
 done
