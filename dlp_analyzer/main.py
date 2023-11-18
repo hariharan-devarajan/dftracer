f7111035f2b4b047e069650efcf5475775defb48
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-11-17 16:25:26 -0800

    improve analyzer

--- dlp_analyzer/main.py
+++ dlp_analyzer/main.py
@@ -634,8 +634,8 @@ def main():
     setup_dask_cluster()
     analyzer = DLPAnalyzer(args.trace)
     analyzer.summary()
-    analyzer.plots.time_bw_timeline(figsize=(8, 4), time_col="io_time")
-    analyzer.plots.xfer_size_timeline(figsize=(4, 4))
+    #analyzer.plots.time_bw_timeline(figsize=(8, 4), time_col="io_time")
+    #analyzer.plots.xfer_size_timeline(figsize=(4, 4))
 
 if __name__ == '__main__':
     main()
