bd66ccf0705a0d76ca0c47359e449dee35afe790
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-11-17 16:19:43 -0800

    improve analyzer

--- dlp_analyzer/dask/scripts/stop_dask_distributed.sh
+++ dlp_analyzer/dask/scripts/stop_dask_distributed.sh
@@ -3,7 +3,7 @@ set -x
 source $HOME/.dlio_profiler/configuration.sh
 export PYTHONPATH=${DLIO_PROFILER_APP}:${PYTHONPATH}
 # This can be set using env variable or arguments to script.
-DLIO_PROFILER_DASK_CONF_NAME=${DLIO_PROFILER_APP}/dlp_analyzer/dask/conf/corona.yaml
+DLIO_PROFILER_DASK_CONF_NAME=${DLIO_PROFILER_APP}/dlp_analyzer/dask/conf/ruby.yaml
 
 # This is start of every script.
 source ${DLIO_PROFILER_APP}/dlp_analyzer/dask/scripts/utils.sh
