41cb05db70d3a0be0a0d6d6299c26a36f7f55fc9
hariharandev1 <hariharandev1@llnl.gov>
2024-06-25 22:41:37 -0700

    changed release to 1.0.0

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="pydftracer",
-    version="0.0.6",
+    version="1.0.0",
     description="I/O profiler for deep learning python apps. Specifically for dlio_benchmark.",
     long_description=long_description,
     long_description_content_type="text/markdown",
