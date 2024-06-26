dc1ce44042e669e6da495f906ca5f8b155c9f155
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-06-26 13:24:07 -0700

    Release v1.0.1 (#104)
    * added scripts for AD (#99)
    
    * Paper/ad ae (#100)
    
    * added scripts for AD
    
    * Improved competitors
    
    * fixed manifest file for pypi source release. (#101)
    
    * release for 1.0.1 (#103)

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
     name="pydftracer",
-    version="1.0.0",
+    version="1.0.1",
     description="I/O profiler for deep learning python apps. Specifically for dlio_benchmark.",
     long_description=long_description,
     long_description_content_type="text/markdown",
