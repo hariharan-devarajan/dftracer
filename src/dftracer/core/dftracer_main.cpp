b9f5dc0087a2ad433a0425f5077c7aa655e6f1f6
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-07-02 21:32:19 -0700

    Update dftracer_main.cpp

--- src/dftracer/core/dftracer_main.cpp
+++ src/dftracer/core/dftracer_main.cpp
@@ -148,9 +148,14 @@ void dftracer::DFTracerCore::initialize(bool _bind, const char *_log_file,
           ssize_t read_bytes = df_read(fd, exec_file_name, DFT_PATH_MAX);
           df_close(fd);
           ssize_t index = 0;
+          size_t parts = 0;
           while (index < read_bytes - 1 && index < DFT_PATH_MAX - 2) {
             if (exec_file_name[index] == '\0') {
               exec_file_name[index] = SEPARATOR;
+              parts++;
+            }
+            if (parts > 1) {
+              exec_file_name[index] = '\0';
             }
             index++;
           }
