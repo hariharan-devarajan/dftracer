d13504de221639bcca3dd6e63a618d778e095d72
OlgaKogiou <44145010+OlgaKogiou@users.noreply.github.com>
2024-07-10 12:03:29 -0400

    Changed start_dask_distributed.sh to check for running scheduler (#118)
    Co-authored-by: Olga Kogiou <kogiou1@corona212.llnl.gov>

--- dfanalyzer/dask/scripts/start_dask_distributed.sh
+++ dfanalyzer/dask/scripts/start_dask_distributed.sh
@@ -12,6 +12,9 @@ case $hostname in
   *"ruby"*)
     DFTRACER_DASK_CONF_NAME=${DFTRACER_APP}/dfanalyzer/dask/conf/ruby.yaml
     ;;
+  "quartz"*)
+    DFTRACER_DASK_CONF_NAME=${DFTRACER_APP}/dfanalyzer/dask/conf/quartz.yaml
+    ;;
 esac
 
 if [[ "$DFTRACER_DASK_CONF_NAME" == "UNSET" ]]; then
@@ -28,8 +31,26 @@ eval $(parse_yaml $DFTRACER_DASK_CONF_NAME DFTRACER_)
 
 source ${DFTRACER_ENV}/bin/activate
 
+rm -rf ${DFTRACER_CONFIG_RUN_DIR}/scheduler_${USER}.json
+
 dask scheduler --scheduler-file ${DFTRACER_CONFIG_RUN_DIR}/scheduler_${USER}.json --port ${DFTRACER_SCHEDULER_PORT} > ${DFTRACER_CONFIG_LOG_DIR}/scheduler_${USER}.log 2>&1 &
 scheduler_pid=$!
 echo $scheduler_pid > ${DFTRACER_CONFIG_RUN_DIR}/scheduler_${USER}.pid
+
+file=${DFTRACER_CONFIG_RUN_DIR}/scheduler_${USER}.json
+timeout=30  # seconds to wait for timeout
+SECONDS=0   # initialize bash's builtin counter 
+
+until [ -s "$file" ] || (( SECONDS >= timeout )); do sleep 1; done
+
+
+if test -f $file;
+then
+   echo "Scheduler with $scheduler_pid is running"
+   # Do something knowing the pid exists, i.e. the process with $PID is running
+else
+   echo "Scheduler with $scheduler_pid failed. Check the ${DFTRACER_CONFIG_LOG_DIR}/scheduler_${USER}.log file"
+   exit 1
+fi
 
 ${DFTRACER_SCHEDULER_CMD} ${DFTRACER_CONFIG_SCRIPT_DIR}/start_dask_worker.sh ${DFTRACER_DASK_CONF_NAME}
